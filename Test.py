125d69e579b63752c8964fb2d85306264921f7d5
vaejiang19960307@gmail.com <vaejiang19960307@gmail.com>
2023-08-07 20:05:58 +0000

    add files

--- Test.py
+++ Test.py
@@ -1,24 +1,77 @@
 # Databricks notebook source
-import os
-import re
-import datetime
 from PyPDF2 import PdfReader
-from typing import List, Tuple, Dict
-
-# COMMAND ----------
 
 from utils.extract_raw import raw_credit_report_data_extraction
 from utils.clean_raw import raw_credit_report_data_cleaning
 
 # COMMAND ----------
 
-# MAGIC %md 
-# MAGIC
-# MAGIC ##### Move ADLS File to DBFS
+from pyspark.sql import functions as F
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql import types as T
 
 # COMMAND ----------
 
-access_target_file_path = r'/Volumes/expense_report/apple_card/reports/Apple Card Statement - May 2023.pdf'
+# MAGIC %md 
+# MAGIC
+# MAGIC ##### Check New File and Write to CheckPoint
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ###### read checkpoint files
+
+# COMMAND ----------
+
+checkpoint_file_path = r'/Volumes/expense_report/apple_card/reports/credit_card_checkpoint.txt'
+
+# COMMAND ----------
+
+with open(checkpoint_file_path, 'r') as file: 
+    checkpoints = file.read().split('\n')
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ###### read all pdfs
+
+# COMMAND ----------
+
+list_pdfs = (
+    spark.
+        sql("LIST '/Volumes/expense_report/apple_card/reports/'").
+        where(
+            F.locate('pdf', F.col('path')) != 0
+        ).
+        select(F.col('path')).
+        collect()
+)
+
+list_pdfs = [x.asDict()['path'] for x in list_pdfs]
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ###### find new files
+
+# COMMAND ----------
+
+new_files = [all_file for all_file in list_pdfs if all_file not in checkpoints]
+print(new_files)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ###### write new files to checkpoint file
+
+# COMMAND ----------
+
+with open(checkpoint_file_path, 'w') as file:
+    file.write('/n'.join(list_pdfs))
 
 # COMMAND ----------
 
@@ -28,25 +81,17 @@ access_target_file_path = r'/Volumes/expense_report/apple_card/reports/Apple Car
 
 # COMMAND ----------
 
+access_target_file_path = r'/Volumes/expense_report/apple_card/reports/Apple Card Statement - May 2023.pdf'
+
+# COMMAND ----------
+
+import os 
+
+os.listdir(r'/Volumes/expense_report/apple_card/reports')
+
+# COMMAND ----------
+
 my_credit_report = PdfReader(access_target_file_path)
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC
-# MAGIC ##### Metadata
-
-# COMMAND ----------
-
-meta_creator = my_credit_report.metadata.creator
-meta_creation_date_raw = my_credit_report.metadata.creation_date_raw
-meta_title = my_credit_report.metadata.title
-
-# COMMAND ----------
-
-print(meta_creator)
-print(meta_creation_date_raw)
-print(meta_title)
 
 # COMMAND ----------
 
---
+++ utils/checkpoint_functions.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple, Dict, NoneType
+
+class checkpoint():
+    def __init__(self):
+        self.checkpoint_file_path = r'/Volumes/expense_report/apple_card/reports/credit_card_checkpoint.txt'
+        self.volumn_path = r'/Volumes/expense_report/apple_card/reports/'
+
+    def find_new_files(self) -> List:
+        existing_files = self.__read_existing_checkpoints()
+        all_files = self.__read_all_pdfs()
+        return [all_file for all_file in list_pdfs if all_file not in checkpoints]
+
+    def append_new_file(self, file_dir: str) -> NoneType:
+        existing_files = self.__read_existing_checkpoints()
+        with open(self.checkpoint_file_path, 'w') as file:
+            file.write('/n'.join(existing_files.append(file_dir)))
+
+    def __read_existing_checkpoints(self) -> NoneType:
+        with open(self.checkpoint_file_path, 'r') as file: 
+            return file.read().split('\n')
+    
+    def __read_all_pdfs(self) -> List:
+        list_pdfs = (
+            spark.
+                sql(f"LIST {self.volumn_path}").
+                where(
+                    F.locate('pdf', F.col('path')) != 0
+                ).
+                select(F.col('path')).
+                collect()
+        )
+
+        return [x.asDict()['path'] for x in list_pdfs]
